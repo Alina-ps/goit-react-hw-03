ee907762994788743ff6b683cb606b4e2adee087
Alina-ps <alinaserg2014@gmail.com>
2024-07-15 16:34:31 +0200

    css added

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -4,7 +4,13 @@ const SearchBox = ({ value, onChange }) => {
   return (
     <div className={s.searchWrapper}>
       <p className={s.searchName}>Find contacts by name</p>
-      <input type="text" value={value} onChange={onChange} />
+      <input
+        className={s.searchInput}
+        type="text"
+        placeholder="Search contacts..."
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
